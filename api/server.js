1ad6cf6b2b804b59c092991416de21e1214815c6
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-02 19:11:47 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -10,6 +10,7 @@ server.use(express.json()) // this teaches express to read JSON from reqs
 // ENDPOINTS
 
 // [GET] / (Hello World endpoint)
+server.get()
 // [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [GET] /api/dogs (R of CRUD, fetch all dogs)
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
