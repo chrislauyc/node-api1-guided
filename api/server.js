bdfbac4f93ca133d2376a6f6b80d684b03166c28
Chris Lau <chrislyc.lau@gmail.com>
2021-08-03 19:13:21 -0600

    endpoints implemented

--- api/server.js
+++ api/server.js
@@ -14,3 +14,87 @@
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // EXPOSING THE SERVER TO OTHER MODULES
+
+// import the server and start it
+const express = require("express");
+const dogModel = require("./dog-model")
+const server = express();
+server.use(express.json());
+
+server.get("/api/dogs",(req,res)=>{
+    dogModel.findAll()
+    .then((dogs)=>{
+        console.log(dogs);
+        res.status(200).json(dogs);
+    })
+    .catch(()=>{
+        res.status(500).send("cannot obtain dogs");
+    })
+});
+server.post("/api/dogs",(req,res)=>{
+    const {name, weight} = req.body;
+    if(name&&weight){
+        dogModel.create(req.body)
+        .then((newDog)=>{
+            res.status(201).json(newDog);
+        })
+        .catch(()=>{
+            res.status(500).json({message:"cannot create dog"});
+        });
+    }
+    else{
+        res.status(400).json({message:"dog must have name and weight"});
+    }
+
+});
+server.get("/api/dogs/:id",(req,res)=>{
+    dogModel.findById(req.params.id)
+    .then((dog)=>{
+        if(dog){
+            res.status(200).json(dog);
+        }
+        else{
+            res.status(404).send({message:"dog is not found"});
+        }
+    })
+    .catch((err)=>{
+        res.status(500).json({message:err.message});
+    });
+    
+
+});
+server.put("/api/dogs/:id",(req,res)=>{
+    const {name, weight} = req.body;
+    if(name&&weight){
+        dogModel.update(req.params.id,req.body)
+        .then((updatedDog)=>{
+            if(updatedDog){
+                res.status(200).json(updatedDog);
+            }
+            else{
+                res.status(404).json({message:"dog is not found"});
+            }
+        })
+        .catch((err)=>{
+            res.status(500).json({message:err.message});
+        })
+    }
+    else{
+        res.status(400).json({message:err.message});
+    }
+});
+server.delete("/api/dogs/:id",(req,res)=>{
+    dogModel.delete(req.params.id)
+    .then((delDog)=>{
+        if(delDog){
+            res.status(200).json(delDog);
+        }
+        else{
+            res.status(404).json({message:"dog is not found"});
+        }
+    })
+    .catch((err)=>{
+        res.status(500).json({message:err.message});
+    })
+});
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// import the server and start it
+
+const server = require("./api/server");
+
+server.listen("5000",()=>console.log(`server is running at 5000`));
